c0284bd059b55234be1380e3fce72762807abdf5
dimndot <93150262+dimndot@users.noreply.github.com>
2023-01-17 19:41:40 +0530

    Add project files.

---
+++ dbo/scripts/Build-Script/DacpacDatabase.sql
@@ -0,0 +1,19 @@
+USE [master]
+GO
+
+/****** Object:  Database [DeploymentDatabase]    Script Date: 1/17/2023 7:32:08 PM ******/
+CREATE DATABASE [DeploymentDatabase]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'DeploymentDatabase', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\DeploymentDatabase.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'DeploymentDatabase_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\DeploymentDatabase_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [DeploymentDatabase].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
